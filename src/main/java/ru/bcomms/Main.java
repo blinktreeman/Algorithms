4a2cfe22415629e9298869f6ee7a9787882da04c
Eugene Elantsev <blinktreeman@gmail.com>
2023-02-02 00:34:47 +0700

    binary tree done

--- src/main/java/ru/bcomms/Main.java
+++ src/main/java/ru/bcomms/Main.java
@@ -26,10 +26,27 @@ public class Main {
          */
 
         RBBinaryTree<Integer> RBBinaryTree = new RBBinaryTree<>();
-        for (int i = 10; i > 0; i--) {
+        for (int i = 1; i < 15; i++) {
             RBBinaryTree.add(i);
         }
         System.out.println(RBBinaryTree);
+        /*
+        Output:
+                  |-- 14 BLACK
+                       |-- 13 RED
+             |-- 12 BLACK
+                       |-- 11 BLACK
+                  |-- 10 RED
+                       |-- 9 BLACK
+        |-- 8 BLACK
+                       |-- 7 BLACK
+                  |-- 6 BLACK
+                       |-- 5 BLACK
+             |-- 4 RED
+                       |-- 3 BLACK
+                  |-- 2 BLACK
+                       |-- 1 BLACK
+         */
     }
 
 }
--- src/main/java/ru/bcomms/tree/RBBinaryTree.java
+++ src/main/java/ru/bcomms/tree/RBBinaryTree.java
@@ -5,39 +5,92 @@ public class RBBinaryTree<T extends Comparable<T>> {
     private final StringBuilder treeVisualization = new StringBuilder();
 
     public void add(T value) {
-        RBBinaryTreeNode<T> node = new RBBinaryTreeNode<>();
-        node.setValue(value);
         if (root == null) {
-            root = node;
+            root = new RBBinaryTreeNode<>();
+            root.setValue(value);
+            root.setColor(NodeColor.BLACK);
         } else {
-            RBBinaryTreeNode<T> currentNode = root;
-            do {
-                if (node.getValue().compareTo(currentNode.getValue()) < 0) {
-                    if (currentNode.getLeft() != null) {
-                        currentNode = currentNode.getLeft();
-                    } else {
-                        currentNode.setLeft(node);
-                        break;
-                    }
-                } else {
-                    if (currentNode.getRight() != null) {
-                        currentNode = currentNode.getRight();
-                    } else {
-                        currentNode.setRight(node);
-                        break;
-                    }
-                }
-            } while (true);
+            add(root, value);
+            root = treeBalancing(root);
         }
+    }
+
+    private void add(RBBinaryTreeNode<T> node, T value) {
+        if (node.getValue().compareTo(value) > 0) {
+            // Если нода не пуста
+            if (node.getLeft() != null) {
+                // Рекурсивно идем в глубину
+                add(node.getLeft(), value);
+                // При каждом выходе из рекурсии балансируем дерево
+                // от нижней ноды к root-у
+                node.setLeft(treeBalancing(node.getLeft()));
+            } else {
+                // Иначе размещаем ноду на пустом месте
+                node.setLeft(new RBBinaryTreeNode<>());
+                node.getLeft().setValue(value);
+                node.getLeft().setColor(NodeColor.RED);
+            }
+        } else {
+            if (node.getRight() != null) {
+                add(node.getRight(), value);
+                node.setRight(treeBalancing(node.getRight()));
+            } else {
+                node.setRight(new RBBinaryTreeNode<>());
+                node.getRight().setValue(value);
+                node.getRight().setColor(NodeColor.RED);
+            }
+        }
+    }
+
+    /*
+    Правила балансировки
+    Если правый ребенок – красный, а левый - черный, то применяем малый правый поворот
+	Если левый ребенок красный и его левый ребенок тоже красный – применяем малый левый поворот
+	Если оба ребенка красные – делаем смену цвета
+	Если корень стал красным – просто перекрашиваем его в черный
+     */
+    private RBBinaryTreeNode<T> treeBalancing(RBBinaryTreeNode<T> node) {
+        boolean needBalancing;
+        do {
+            needBalancing = false;
+            // Если правый ребенок – красный,
+            if (node.getRight() != null && node.getRight().getColor().equals(NodeColor.RED)
+                    // а левый - черный, то применяем малый правый поворот
+                    && (node.getLeft() == null || node.getLeft().getColor().equals(NodeColor.BLACK))) {
+                node = rightTurn(node);
+                // Нужна проверка, возможна дальнейшая балансировка
+                needBalancing = true;
+            }
+            // Если левый ребенок красный
+            if (node.getLeft() != null && node.getLeft().getColor().equals(NodeColor.RED)
+                    // и его левый ребенок тоже красный – применяем малый левый поворот
+                    && node.getLeft().getLeft() != null
+                    && node.getLeft().getLeft().getColor().equals(NodeColor.RED)) {
+                node = leftTurn(node);
+                needBalancing = true;
+            }
+            // Если оба ребенка красные – делаем смену цвета
+            if (node.getLeft() != null && node.getLeft().getColor().equals(NodeColor.RED)
+                    && node.getRight() != null && node.getRight().getColor().equals(NodeColor.RED)) {
+                colorChange(node);
+                needBalancing = true;
+            }
+            // Если корень стал красным – просто перекрашиваем его в черный
+            if (node == root && node.getColor().equals(NodeColor.RED)) {
+                node.setColor(NodeColor.BLACK);
+                //needBalancing = true;
+            }
+        } while (needBalancing);
+        return node;
     }
 
     // Левый малый поворот
     private RBBinaryTreeNode<T> leftTurn(RBBinaryTreeNode<T> node) {
         RBBinaryTreeNode<T> leftChild = node.getLeft();
         RBBinaryTreeNode<T> betweenParentAndChild = node.getLeft().getRight();
-        node.setLeft(betweenParentAndChild);
         leftChild.setRight(node);
         leftChild.setColor(node.getColor());
+        node.setLeft(betweenParentAndChild);
         node.setColor(NodeColor.RED);
         return leftChild;
     }
@@ -46,11 +99,18 @@ public class RBBinaryTree<T extends Comparable<T>> {
     private RBBinaryTreeNode<T> rightTurn(RBBinaryTreeNode<T> node) {
         RBBinaryTreeNode<T> rightChild = node.getRight();
         RBBinaryTreeNode<T> betweenParentAndChild = node.getRight().getLeft();
-        node.setRight(betweenParentAndChild);
         rightChild.setLeft(node);
         rightChild.setColor(node.getColor());
+        node.setRight(betweenParentAndChild);
         node.setColor(NodeColor.RED);
         return rightChild;
+    }
+
+    // Смена цвета
+    private void colorChange(RBBinaryTreeNode<T> node) {
+        node.getLeft().setColor(NodeColor.BLACK);
+        node.getRight().setColor(NodeColor.BLACK);
+        node.setColor(NodeColor.RED);
     }
 
     @Override
@@ -64,9 +124,14 @@ public class RBBinaryTree<T extends Comparable<T>> {
 
     public void buildTreeVisualization(String prefix, RBBinaryTreeNode<T> node) {
         if (node != null) {
-            buildTreeVisualization(prefix + "    ", node.getRight());
-            treeVisualization.append(prefix).append("|-- ").append(node.getValue()).append("\n");
-            buildTreeVisualization(prefix + "    ", node.getLeft());
+            buildTreeVisualization(prefix + "     ", node.getRight());
+            treeVisualization.append(prefix)
+                    .append("|-- ")
+                    .append(node.getValue())
+                    .append(" ")
+                    .append(node.getColor().toString())
+                    .append("\n");
+            buildTreeVisualization(prefix + "     ", node.getLeft());
         }
     }
 
